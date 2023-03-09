54614270fd9463bd2e09f187bea9adfab652c76d
StevenMODIMO <stevepetes26@gmail.com>
2023-03-09 13:07:04 +0300

    Initial Commit

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,11 +1,12 @@
 const menu = document.getElementById("menu-bar");
-      const ul = document.querySelector("ul");
-      menu.addEventListener("click", () => {
-        if (ul.classList.contains("hide")) {
-          ul.classList.remove("hide");
-          ul.classList.add("show");
-        } else {
-          ul.classList.remove("show");
-          ul.classList.add("hide");
-        }
-      });
+const ul = document.querySelector("ul");
+
+menu.addEventListener("click", () => {
+  if (ul.classList.contains("hide")) {
+    ul.classList.remove("hide");
+    ul.classList.add("show");
+  } else {
+    ul.classList.remove("show");
+    ul.classList.add("hide");
+  }
+});
